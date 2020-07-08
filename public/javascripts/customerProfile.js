30af87ad620e56b22d2b785787813bb58a5c395b
Ian Goodnight <goodnight.ianb@gmail.com>
2020-07-08 17:02:09 -0400

    refactored customerProfile page logic.  Much nicer

--- public/javascripts/customerProfile.js
+++ public/javascripts/customerProfile.js
@@ -1,19 +1,136 @@
 $( document ).ready(function() {
-
+	//
+	// Globals
+	//
+	// Select options for onClick population
 	const laminateOptions = ['Matte', 'High-gloss'];
 	const machineOptions = ['Metas', 'Plotter'];
+	// Populated by filterStatuses()
 	let statusOptions = [];
+	//
+	// Functions
+	//
+	// IIFE to populate statusOptions
 	(async function filterStatuses() {
 		await call.getStatuses()
-			.then(res => {
-				console.log("REs: ", res);		
+			.then(res => {		
 				res.map(status => {
-					console.log('Status: ', status);
 					statusOptions.push(status.status);
 				});
 			});
+		let statusList = [];
+		statusOptions.forEach(status => {
+			let openLink = '<a class="dropdown-item status" href="#">';
+			let closeLink = '</a>';
+			statusList.push(openLink, status, closeLink);
+		});
+		let html = statusList.join('');
+		$('#dropdown-statuses').html(html);
 	})();
-    // Create onChange event bound to contenteditable.
+    // Select all contenteditable onFocus
+    function selectElementContents(el) {
+        let range = document.createRange();
+        range.selectNodeContents(el);
+        let sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+    };
+    // Set all inputs back to their default
+    function resetInputs() {
+    	// Logic for customer form
+    	let rows = $('div.card div.row');
+    	let spans = $('span.edit');
+    	rows.removeClass('target');
+    	spans.removeClass('selectAll');
+    	spans.attr('contenteditable', 'false');
+        $('#customer-email-link').bind('click'); 
+    	$('div.notes-entry').fadeOut(600);
+    	$('#notes').fadeIn(600);
+    	// Logic for order form
+        $('select.temporary-input').hide();
+        $('span.input-swap').show();
+    	$('tr.notes textarea').hide();
+    	$('tr.notes span.edit').show();
+    	$('table.nested, div.card').removeClass('editing');
+    };
+    // Build Select inputs where contenteditable is a PITA
+    function buildSelect(target, optionsArr) {
+    	let options = ['<select class="temporary-input">'];
+    	let current = $(target).text().trim();
+    	let defaultOption;
+    	current === '' ? defaultOption = '<option value="">Select</option>'
+    		: defaultOption = '<option value="' + current + '">' + current + '</option>';
+    	options.push(defaultOption);
+    	optionsArr.forEach(option => {
+    		let html;
+    		option !== current ? html = '<option value="' + option + '">' + option + '</option>'
+    			: html ='';
+    		options.push(html);
+    	});
+    	options.push('</select>');
+    	let el = options.join('');
+    	return el;
+    };
+    // Handler for "pencil" click on a future select input
+    function selectHandler(row, target, selectElement) {
+    	if ($(row).find('select.temporary-input').length === 0) {
+            $(target).hide();
+            $(target).closest('td').append(selectElement);    		
+    	} else {
+        	$(target).hide();
+        	$(target).closest('td').find('select').show(); 		
+    	}
+    };
+    // Handle toolbar display
+    function displayToolbar(save = false, status = false) {
+    	let saveButtons = $('.save');
+ 		$('#toolbar').fadeIn(600);
+ 		saveButtons.attr('disabled', true);
+ 		saveButtons.addClass('disabled');
+ 		save ? enableSaveAll() : false; 
+ 		status ? enableChangeStatus() : false;   	
+    };
+    // Hide toolbar if necessary
+    function hideToolbar() {
+    	let saveButtons = $('.save');
+ 		$('#toolbar').fadeOut(600);
+ 		saveButtons.attr('disabled', false);
+ 		saveButtons.removeClass('disabled');
+ 		enableSaveAll(false); 
+ 		enableChangeStatus(false);   	
+    };
+    // Unlock the save all button from the toolbar
+    function enableSaveAll(toggle = true) {
+    	let toolbarSave = $('#save-all');
+    	if (toggle) {
+ 			toolbarSave.attr('disabled', false);
+ 			toolbarSave.removeClass('disabled');    		
+    	} else {
+  			toolbarSave.attr('disabled', true);
+ 			toolbarSave.addClass('disabled');   		
+    	};
+    };
+    // Unlock the status change dropdown from the toolbar
+    function enableChangeStatus(toggle = true) {
+    	let statusChange = $('#statusChange');
+    	if (toggle) {
+ 			statusChange.attr('disabled', false);
+ 			statusChange.removeClass('disabled');    		
+    	} else {
+ 			statusChange.attr('disabled', true);
+ 			statusChange.addClass('disabled');
+    	};
+    };
+    function countSaveButtons() {
+    	let visibleSaveButtons = $('.save:visible');
+    	let check;
+    	visibleSaveButtons.length > 1 ? check = true: check = false;
+    	return check;   	
+    };
+    //
+    // Event Handlers
+    //
+    // Create onChange event bound to contenteditable and textareas.
     $('body').on('focus', '[contenteditable]', function() {
         const $this = $(this);
         $this.data('before', $this.html());
@@ -24,14 +141,18 @@ $( document ).ready(function() {
             $this.trigger('change');
         };
     });
-    // Select all contenteditable onFocus
-    function selectElementContents(el) {
-        let range = document.createRange();
-        range.selectNodeContents(el);
-        let sel = window.getSelection();
-        sel.removeAllRanges();
-        sel.addRange(range);
-    };
+    // Reset input when clicking outside of form
+    $('body').mousedown(e => {
+        let target = $(e.target);
+        if (target.closest('.editing').length === 0 ) {
+        	resetInputs();
+        };
+    });
+    // Hide and show toolbar based on body language
+    $('body, textarea').on('change', function(e) {
+    	let count = countSaveButtons();
+		count ? displayToolbar(true): false;
+    });
     // Define Enter key behavior within contenteditable
     $('span.edit').on('keydown', e => {
         if (e.keyCode == 13 && !e.ctrlKey) {
@@ -73,9 +194,8 @@ $( document ).ready(function() {
     		let key = $( el ).closest('tr').data('key');
     		let val = $( el ).text().trim();
     		orderDetails[key] = val;
-    	})
-    	console.log(orderDetails);
-    })
+    	});
+    });
     // Handle magnifying glass onClick (Display order details)
     $('.open-order').on('click', (e) => {
         let target = e.target;
@@ -96,11 +216,9 @@ $( document ).ready(function() {
                 'font-weight': 'normal',
                 'font-size': 'inherit'
             });
-        }
-        console.log(id);
+        };
     });
     // Keep state updated with values
-    // To DO: Extend this functionality.
     $('tr.select').on('change', 'select.temporary-input', (e) => {
         let newValue = $(e.target).val();
         $(e.target).closest('td').find('span.edit').text(newValue);
@@ -108,24 +226,6 @@ $( document ).ready(function() {
         let table = $(e.target).closest('table');
         $(table).find('button.save-order').fadeIn(600);
     });
-    // Build Select inputs where contenteditable is a PITA
-    function buildSelect(target, optionsArr) {
-    	let options = ['<select class="temporary-input">'];
-    	let current = $(target).text().trim();
-    	let defaultOption;
-    	current === '' ? defaultOption = '<option value="">Select</option>'
-    		: defaultOption = '<option value="' + current + '">' + current + '</option>';
-    	options.push(defaultOption);
-    	optionsArr.forEach(option => {
-    		let html;
-    		option !== current ? html = '<option value="' + option + '">' + option + '</option>'
-    			: html ='';
-    		options.push(html);
-    	});
-    	options.push('</select>');
-    	let el = options.join('');
-    	return el;
-    };
     // Handles textarea magic
     $('#notes-area').on('keyup', e => {
         let note = $('#notes-area').val();
@@ -141,32 +241,13 @@ $( document ).ready(function() {
         let table = $(e.target).closest('table');
         $(table).find('button.save-order').fadeIn(600);
     });
-    // Handler for "pencil" click on a future select input
-    function selectHandler(row, target, selectElement) {
-    	if ($(row).find('select.temporary-input').length === 0) {
-            $(target).hide();
-            $(target).closest('td').append(selectElement);    		
-    	} else {
-    		console.log("Fallback...");
-        	$(target).hide();
-        	$(target).closest('td').find('select').show(); 		
-    	}
-    };
-    //
-    // Needs Refactoring
-    //
-
     // Handle Order Edits through customer profile
     $('table.nested tbody tr td i.fa-pencil-alt').on('click', e => {
-        console.log("Edit order...");
-        let spans = $('table span.edit');
-        spanDecorator(spans);
-        $('select.temporary-input').hide();
-        $('span.input-swap').show();
-        $(spans).removeClass('selectAll'); 
+        resetInputs(); 
         let row = $(e.target).closest('tr');
         let target = $(row).find('span.edit');
         let selectElement;
+        $(target).closest('table.nested').addClass('editing');
         switch ($(row).data('key')) {
         	// Handle any special cases in the "Order" form
             case 'laminate':
@@ -186,11 +267,10 @@ $( document ).ready(function() {
             	let text = $(target).text().trim();
             	let textArea = $(target).closest('tr').find('textarea');
             	$(textArea).val(text);
-            	$(textArea).show();
+            	$(textArea, target).toggle();
             	break;
             default:
                 $(target).addClass('selectAll');
-                $(target).css('background-color', '#fff');
                 $(target).attr('contenteditable', 'true');
                 $(target).focus().select();
                 requestAnimationFrame(() => {
@@ -198,28 +278,17 @@ $( document ).ready(function() {
                 });
                 break;
         };
-
     });
-
+    // Handle customer edits
     $('#customer-info i.fa-pencil-alt').on('click', e => {
-        let spans = $('span.edit');
-        $(spans).removeClass('selectAll');
-        let rows = $(spans).closest('div.row');
+    	resetInputs();
         let target = $(e.target).closest('div.row');
         let edit = $(target).find('span.edit');
-        $(rows).css('background-color', '#fff');
-        $(spans).attr('contenteditable', 'false');
         $('#customer-email-link').unbind('click');
+        $('div.card').addClass('editing');
         if (!$(target).hasClass('notes')) {
-            $('span#notes').text($('div.notes-entry div textarea').val());
-            $('div.notes-entry').fadeOut(500);
-            $('span#notes').fadeIn(1600);            
-            $(target).css({
-                'background-color': '#f8f9fa'
-            });
-            $(edit).css({
-                'background-color': '#fff'
-            });
+            $('span#notes').text($('div.notes-entry div textarea').val());         
+            $(target).addClass('target');
             $(edit).attr('contenteditable','true');
             $(edit).addClass('selectAll');
             $(edit).focus().select();
@@ -235,40 +304,19 @@ $( document ).ready(function() {
             $('div.notes-entry textarea').val(currentNotes);
         };
     });
-
-    function spanDecorator(spans, noteToggle) {
-    	// if (noteToggle && noteTogggle !== false) {
-    	// 	$('textarea').hide();    		
-    	// };
-    	$(spans).show();
-        $(spans).attr('contenteditable', 'false');
-        $(spans).css('background-color', 'inherit');    	
-    };
-    //
-    // Maybe we need to clean this up.  Seems to add some confusion to customer edit form
-    //
-    $('body').mousedown(e => {
-        console.log('Body event');
-        let target = $(e.target);
-        let spans;
-        if (target.closest('div.card div.row').length === 0 ) {
-        	console.log("Customer");
-            spans = $('div.card span.edit');
-            let rows = $(spans).closest('div.row');
-            $(rows).css('background-color', '#fff');
-            spanDecorator(spans);
-            $('span#notes').text($('div.notes-entry div textarea').val());
-            $('div.notes-entry').fadeOut(600);
-            $('span#notes').fadeIn(600);
-            $('#customer-email-link').bind('click');
-			console.log(toggle);
-        };
-        if (target.closest('table.nested').length === 0) {
-            spans = $('table span.edit');
-            spanDecorator(spans);
-            $('.temporary-input').hide(); 
-            $('span.input-swap').show(); 
-            console.log(toggle);              
-        };
+    // To Do:
+    // Save All
+    // Select All
+    // Bulk Edits
+    // Save orders
+    $('#order-select-all').on('change', function(e) {
+    	if (this.checked) {
+    		$('input[type="checkbox"]').prop('checked', true);
+    		displayToolbar(false, true);
+    	} else {
+    		let count = countSaveButtons();
+    		count ? true: hideToolbar();
+    		$('input[type=checkbox]').prop('checked', false);
+    	};
     });
 });
